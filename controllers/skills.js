a1f12fa041e8f5a5212c3e3b92da78c8e2f11278
Donald Adams-Sellin <snave1993@hotmail.com>
2021-12-03 17:33:27 -0500

    set up files

---
+++ controllers/skills.js
@@ -0,0 +1,20 @@
+const Skill = require('../models/skill');
+
+module.exports = {
+    index,
+    show
+};
+
+function index (req ,res){
+    let skills = Skill.getAll();
+    res.render('skills/index',{
+        skills
+    });
+}
+
+function show (req, res){
+    let skill = Skill.getOne(req.params.id);
+    res.render('skills/show',{
+        skill
+    });
+}
